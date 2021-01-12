3a7199cb630b5ddae794eb766bed8bec46a6d019
Baconnier Anaïs <abaconnier@iutbayonne.univ-pau.fr>
2021-01-12 13:21:20 +0100

    Modification de l'url de la page d'accueil

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/accueil", name="proStage_accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
+            'controller_name' => 'Controleur proStage',
         ]);
     }
 }
