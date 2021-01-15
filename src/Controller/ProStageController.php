2aa0dd5db2e444ccb2f7f9bfc2aa494ece893790
Baconnier Anaïs <abaconnier@iutbayonne.univ-pau.fr>
2021-01-15 12:00:35 +0100

    Modification des routes

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,10 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
+    /**
+     * @Route("/accueil", name="proStage_accueil")
+     */
+
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig');
     }
+
+    /**
+     * @Route("/ressource/123", name="proStage_ressource123")
+     */
 
     public function afficherRessources(): Response
     {
